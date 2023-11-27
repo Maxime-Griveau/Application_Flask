587ae1f01528a0449d39da8077037db004414e8e
Maxime-Griveau <griveaumaxime@gmail.com>
2023-11-27 11:37:33 +0100

    mise à jour de l'application avec changement de la version

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,1 @@
-ma_variable = 25 
+version = "version 0.0.1" #Version de notre application Flask (elle sera appelée dans congfig, lui même appelée par notre application)
--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import dotenv #POUR INSTALLER DOTENV IL FAUT FAIRE : pip install python-dotenv
 import os
 
-version = "version 0.0.1"
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 dotenv.load_dotenv(os.path.join(BASE_DIR, ".env"))
 
@@ -9,5 +9,6 @@ dotenv.load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 class Config():
     DEBUG = os.environ.get("DEBUG")
+
     
 
