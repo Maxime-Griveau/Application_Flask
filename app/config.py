c09a33849759800ff644adf5b7e43105e1ba69fa
Maxime-Griveau <griveaumaxime@gmail.com>
2023-11-27 11:42:52 +0100

    test

--- app/config.py
+++ app/config.py
@@ -2,8 +2,8 @@ import dotenv #POUR INSTALLER DOTENV IL FAUT FAIRE : pip install python-dotenv
 import os
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-dotenv.load_dotenv(os.path.join(BASE_DIR, ".env"))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) #appel de notre fichier actuel (os va trouver son chemin à chaque fois pour éviter qu'il ne saute si on le modifie)
+dotenv.load_dotenv(os.path.join(BASE_DIR, ".env"))#Charge .env en tant que Base_Dir donc .env sera lié à config sans que personne ne puisse savoir d'où provient .env (sécurité)
 
 
 
