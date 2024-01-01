992d8116e2730f2ee6c2316f9213f755623246a7
Maxime-Griveau <griveaumaxime@gmail.com>
2024-01-01 16:14:45 +0100

    Comit initial de l'application

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+import pymysql #dépendence dont a besoin Flask pour faire tourner une base MySql
+
+app = Flask(
+    __name__, 
+    template_folder='templates',
+    static_folder='statics')
+app.config.from_object(Config)
+
+db = SQLAlchemy(app) #database liée à notre application (instanciée)
+
+
+
+from .routes import generales
